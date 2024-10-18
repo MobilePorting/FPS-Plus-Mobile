bc244ca37a4af911d7aa11c9e9d036c98a5aa1fd
Lily <egzozu.be.bas@gmail.com>
2024-10-18 19:27:20 +0300

    Update Binds.hx

--- source/Binds.hx
+++ source/Binds.hx
@@ -337,7 +337,6 @@ class Binds
     
     public static var isInSubstate:Bool = false;
     public static var requestedInstance:Dynamic = (isInSubstate) ? MusicBeatSubstate.instance : MusicBeatState.instance;
-    public var mobileC:Bool = (config.Config.mobileCAlpha != null && config.Config.mobileCAlpha <= 0) ? false : true;
 
     inline static public function pressedMobileCOnly(input:String){
         var p:Bool = false;
