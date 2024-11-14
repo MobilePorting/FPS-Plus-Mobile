3b8c345aa9ce83004b129d97237b0ee9b8a1bba8
Lily <egzozu.be.bas@gmail.com>
2024-11-14 18:02:38 +0300

    license our classes

--- source/mobile/scalemodes/MobileScaleMode.hx
+++ source/mobile/scalemodes/MobileScaleMode.hx
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2024 Mobile Porting Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package mobile.scalemodes;
 
 #if mobile
@@ -5,6 +22,10 @@ import flixel.FlxG;
 import config.Config;
 import flixel.system.scaleModes.BaseScaleMode;
 
+/**
+ * ...
+ * @author: Karim Akra
+ */
 class MobileScaleMode extends BaseScaleMode {
 	public static var allowWideScreen(default, set):Bool = true;
 
